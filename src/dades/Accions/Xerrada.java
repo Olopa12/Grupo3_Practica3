7b9de7b07260d59ffadc923a81d86c72b22dc2b1
Geckgecky <neobasku@gmail.com>
2024-12-30 21:15:25 +0100

    Update Xerrada.java

--- src/dades/Accions/Xerrada.java
+++ src/dades/Accions/Xerrada.java
@@ -1,61 +1,110 @@
 package dades.Accions;
 
+import dades.Associacions.*;
+import dades.Membres.*;
+
 /**
- * Subclasse Xerrada.
+ * La classe Xerrada és una subclasse de la classe Accio.
+ * En aquesta classe guardarem la data en què s’ha fet la xerrada, les dades dels membres que l’han impartit (podem suposar un màxim prefixat, inicialment a 3).
+ * També guardarem quants assistents ha tingut la xerrada i les valoracions que aquests han fet (les valoracions estaran en l’escala [0-10]). 
+ *
+ * @author Nermin Tribak, Sara Tribak
+ * @version 1.0
  */
+
 public class Xerrada extends Accio {
-    private String dataXerrada; // Data en què es farà la xerrada
-    private String[] membresImpartidors; // Membres que imparteixen la xerrada
-    private int assistents; // Nombre d'assistents
-    private int[] valoracions; // Valoracions fetes pels assistents
-    private int numMembres;
-    private int numValoracions;
+    private String data; // La data en què es farà la xerrada
+    private Membres[] membresImpartidors; // Membres impartidors de la xerrada
+    private int numAssistents; // Nombre d'assistents
+    private int[] valoracions; //  Valoració dels assistents
+    private int valoracionsCount; // Valoracions fins a la data
+    private static final int MAX_MEMBRES = 3; //Nombre màxim de membres que puguin impartir xerrada
+    private static final int MAX_VALORACIONS = 100; // Màxim de valoracions 
 
-    public Xerrada(String titol, String[] associacions, String responsable, String dataXerrada) {
-        super(titol, associacions, responsable);
-        this.dataXerrada = dataXerrada;
-        this.membresImpartidors = new String[3]; // Fins a 3 membres
-        this.assistents = 0;
-        this.valoracions = new int[100]; // Fins a 100 valoracions
-        this.numMembres = 0;
-        this.numValoracions = 0;
+    public Xerrada(String titol, Associacio associacio, Membres responsable, String data) {
+        super(titol, associacio, responsable);
+        this.data = data;
+        this.membresImpartidors = new Membres[MAX_MEMBRES];
+        this.numAssistents = 0;
+        this.valoracions = new int[MAX_VALORACIONS]; // Estableixem un màxim de 100 valoracions
+        this.valoracionsCount = 0;
     }
 
-    public String getDataXerrada() {
-        return dataXerrada;
-    }
-
-    public String[] getMembresImpartidors() {
-        String[] impartidors = new String[numMembres];
-        System.arraycopy(membresImpartidors, 0, impartidors, 0, numMembres);
-        return impartidors;
-    }
-
-    public int getAssistents() {
-        return assistents;
-    }
-
-    public int[] getValoracions() {
-        int[] numV = new int[numValoracions];
-        System.arraycopy(valoracions, 0, numV, 0, numValoracions);
-        return numV;
-    }
-
-    public void afegirMembreImpartidor(String membre) {
-        if (numMembres < 3) {
-            membresImpartidors[numMembres++] = membre;
+    // Mètode per afegir un membre impartidor
+    public boolean afegirMembreImpartidor(Membres membre) {
+        if (membre == null) {
+            return false; // Si el membre és null, no s'afegeix
         }
+        for (int i = 0; i < membresImpartidors.length; i++) {
+            if (membresImpartidors[i] == null) {
+                membresImpartidors[i] = membre;
+                return true; // Membre afegit correctament
+            }
+        }
+        return false; // Si ja hi ha 3 membres, no s'afegeix més
     }
 
+    // Mètode per afegir assistents
     public void afegirAssistents(int nombre) {
         if (nombre >= 0) {
-            assistents += nombre;
+            numAssistents += nombre; // Afegim el nombre d'assistents
         }
     }
 
+    // Mètode per afegir valoracions
     public void afegirValoracio(int valoracio) {
-        if (valoracio >= 0 && valoracio <= 10 && numValoracions < valoracions.length) {
-            valoracions[numValoracions++] = valoracio;
+        if (valoracio >= 0 && valoracio <= 10 && valoracionsCount < valoracions.length) {
+            valoracions[valoracionsCount++] = valoracio; // Afegim la valoració a l'array
         }
     }
+
+    // Mètode per calcular la valoració mitjana
+    public double getValoracioMitjana() {
+        if (valoracionsCount == 0) {
+            return 0; // Si no hi ha valoracions, retornem 0
+        }
+        int suma = 0;
+        for (int i = 0; i < valoracionsCount; i++) {
+            suma += valoracions[i];
+        }
+        return (double) suma / valoracionsCount;
+    }
+
+    // Getter per obtenir la data de la xerrada
+    public String getData() {
+        return data;
+    }
+
+    // Getter per obtenir els membres impartidors
+    public Membres[] getMembresImpartidors() {
+        return membresImpartidors;
+    }
+
+    // Getter per obtenir el nombre d'assistents
+    public int getNumAssistents() {
+        return numAssistents;
+    }
+
+    // Getter per obtenir les valoracions fetes
+    public int[] getValoracions() {
+        int[] valoracionsCopies = new int[valoracionsCount];
+        System.arraycopy(valoracions, 0, valoracionsCopies, 0, valoracionsCount);
+        return valoracionsCopies;
+    }
+
+    // Mètode toString per representar la xerrada
+    @Override
+    public String toString() {
+        String impartidors = "";
+        for (Membres membre : membresImpartidors) {
+            if (membre != null) {
+                impartidors += membre.getAlias() + " "; 
+            }
+        }
+        return "Data=" + data +
+               ", Membres Impartidors=" + impartidors.trim() +
+               ", Assistents=" + numAssistents +
+               ", Valoració Mitjana=" + getValoracioMitjana();
+    }
+    
 }
