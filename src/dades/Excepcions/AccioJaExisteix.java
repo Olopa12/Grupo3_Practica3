31149b04982f8bbabcaf8047afd3addb56a3b50e
Geckgecky <neobasku@gmail.com>
2025-01-04 14:00:22 +0100

    Update AccioJaExisteix.java

--- src/dades/Excepcions/AccioJaExisteix.java
+++ src/dades/Excepcions/AccioJaExisteix.java
@@ -1,15 +1,9 @@
 package dades.Excepcions;
 
-/**
- * Excepción que se lanza cuando una acción ya existe en la lista.
- */
 public class AccioJaExisteix extends Exception {
-    /**
-     * Constructor de la excepción.
-     * @param missatge El mensaje que describe la excepción.
-     */
+  
     public AccioJaExisteix(String missatge) {
         super(missatge);
-        System.out.println("\nL'acció amb codi '" + missatge + "' ja existeix a la llista."); // Pasa el mensaje al constructor de Exception
+        System.out.println("\nL'acció amb codi '" + missatge + "' ja existeix a la llista."); 
     }
 }
