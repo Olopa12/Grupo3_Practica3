0ce9e0a4ab7f3610ea4740bc6a0c78e37c1f5856
AlexRadu687 <160734288+AlexRadu687@users.noreply.github.com>
2025-01-06 13:02:37 +0100

    actualizado TestLlistaAssociacions.java, añadido implements Serializable a Membres.java y a Data.java

--- src/TestErrores/Associacions/TestLlistaAssociacions.java
+++ src/TestErrores/Associacions/TestLlistaAssociacions.java
@@ -2,11 +2,10 @@ package TestErrores.Associacions;
 
 import java.io.IOException;
 
-import TestErrores.Associacions.TestAssociacio.TestMembre;
 import Utilitats.Data;
 import dades.Associacions.Associacio;
 import dades.Associacions.LlistaAssociacions;
-import dades.Membres.Membres;
+import dades.Membres.Alumnes;
 
 /**
  * Classe TestLlistaAssociacio que representa els testes fer per comprovar que
@@ -18,6 +17,8 @@ import dades.Membres.Membres;
 public class TestLlistaAssociacions {
     public static void main(String[] args){
         validarAfegirAssociacio();
+        comprovaBuscarAssociacio();
+        comprovaExisteixAssociacio();
         comprovarFitxerBinari();
     }
     private static void validarAfegirAssociacio(){
@@ -33,8 +34,8 @@ public class TestLlistaAssociacions {
 
         System.out.println("Afegim una associacio:\n");
 
-        Membres membre1 = new TestMembre("Joan123", "joan@etse.com", new Data(10, 1, 2024));
-        Membres membre2 = new TestMembre("Marc321", "correu@exemple.com", new Data(1, 5, 2022));
+        Alumnes membre1 = new Alumnes("Joan123", "joan@etse.com", new Data(10, 1, 2024), "Informatica", 3, true);
+        Alumnes membre2 = new Alumnes("Marc321", "correu@exemple.com", new Data(1, 5, 2022), "Informatica", 3, true);
         Associacio asociacioPaelles = new Associacio("PaellersURV", "paellersurv@urv.cat", "GEI", membre1.copia(), membre1.copia(), membre1.copia());
         llistaAs.afegirAssociacio(asociacioPaelles);
         System.out.println(llistaAs.toString());
@@ -45,7 +46,63 @@ public class TestLlistaAssociacions {
         System.out.println(llistaAs.toString());
     }
 
+    private static void comprovaBuscarAssociacio(){
+        System.out.println("\n\nValidem buscar si esta una associacio a la llista d'associacions.\n");
+
+        LlistaAssociacions llistaAs = new LlistaAssociacions(10);
+        Alumnes membre1 = new Alumnes("Joan123", "joan@etse.com", new Data(10, 1, 2024), "Informatica", 3, true);
+        Alumnes membre2 = new Alumnes("Marc321", "correu@exemple.com", new Data(1, 5, 2022), "Informatica", 3, true);
+        Associacio asociacioPaelles = new Associacio("PaellersURV", "paellersurv@urv.cat", "GEI", membre1.copia(), membre1.copia(), membre1.copia());
+        llistaAs.afegirAssociacio(asociacioPaelles);
+        Associacio asociacioPescaders = new Associacio("PescadersURV", "pescadersurv@urv.cat", "BioGEI", membre2.copia(), membre2.copia(), membre2.copia());
+        llistaAs.afegirAssociacio(asociacioPescaders);
+        System.out.println("\nCas 1:");
+        Associacio a = llistaAs.buscarAssociacio("PescadersURV");
+        if(a == null){
+            System.out.println("\nAssociacio no esta");
+        } else{
+            System.out.println(a);
+        }
+
+        System.out.println("\nCas 2:");
+        a = llistaAs.buscarAssociacio("noExististeix");
+        if(a == null){
+            System.out.println("\nAssociacio no esta");
+        } else{
+            System.out.println(a);
+        }
+    }
+
+    private static void comprovaExisteixAssociacio(){
+        System.out.println("\n\nValidem comprovar si existeix una associacio a la llista d'associacions.\n");
+
+        LlistaAssociacions llistaAs = new LlistaAssociacions(10);
+        Alumnes membre1 = new Alumnes("Joan123", "joan@etse.com", new Data(10, 1, 2024), "Informatica", 3, true);
+        Alumnes membre2 = new Alumnes("Marc321", "correu@exemple.com", new Data(1, 5, 2022), "Informatica", 3, true);
+        Associacio asociacioPaelles = new Associacio("PaellersURV", "paellersurv@urv.cat", "GEI", membre1.copia(), membre1.copia(), membre1.copia());
+        llistaAs.afegirAssociacio(asociacioPaelles);
+        Associacio asociacioPescaders = new Associacio("PescadersURV", "pescadersurv@urv.cat", "BioGEI", membre2.copia(), membre2.copia(), membre2.copia());
+        llistaAs.afegirAssociacio(asociacioPescaders);
+        System.out.println("\nCas 1:");
+        boolean existeix = llistaAs.existeixAssociacio("PescadersURV");
+        if(existeix){
+            System.out.println("\nExisteix l'associacio.");
+        } else{
+            System.out.println("\nNo existeix l'associacio.");
+        }
+
+        System.out.println("\nCas 2:");
+        existeix = llistaAs.existeixAssociacio("noExisteix");
+        if(existeix){
+            System.out.println("\nExisteix l'associacio.");
+        } else{
+            System.out.println("\nNo existeix l'associacio.");
+        }
+    }
+
     private static void comprovarFitxerBinari(){
+        System.out.println("\n\nValidem crear i llegir un fitxer binari amb l'informacio de la LlistaAssociacions.\n");
+
         LlistaAssociacions llistaAs = new LlistaAssociacions(10);
         
         Associacio paellersAssociacio = new Associacio("PaellersURV", "paellersAssociacio@urv.cat", "GEI");
